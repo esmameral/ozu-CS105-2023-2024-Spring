43f183463302ee4341b7bcef5ddb6711a0318f9c
esmameral <esma.meral@gmail.com>
2024-02-13 19:25:02 +0300

    Week1-First Project

---
+++ FirstJavaProject/src/week1/examples/HelloJava.java
@@ -0,0 +1,19 @@
+package week1.examples;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		double radius=4.5;
+				
+		int height=10;
+		double PI=3.14;
+		
+		double volumeSphere=4*PI*radius*radius*radius/3;
+		double volumeCylinder=PI*radius*radius*height;
+		System.out.println(volumeSphere);
+		System.out.println(volumeCylinder);
+
+	}
+
+}
+ 
