14aa0062b9580f7d7218509138e855dc5575b044
esmameral <esma.meral@gmail.com>
2024-02-14 15:23:03 +0300

    Question1 solution added

---
+++ FirstJavaProject/src/week1/examples/Question1_Average.java
@@ -0,0 +1,21 @@
+package week1.examples;
+
+public class Question1_Average {
+
+	public static void main(String[] args) {
+		int a=1;
+		int b=2;
+		int c=2;
+		double average=(a+b+c)/3.0;
+		//Second way: first a+b+c will be completed, then result will be 
+		//converted to double. After then division operator will be applied
+		//double average=(double)(a+b+c)/3;
+		System.out.println(average);
+		
+		
+		//double average=(double)(a+b+c)/3;
+		
+
+	}
+
+}
