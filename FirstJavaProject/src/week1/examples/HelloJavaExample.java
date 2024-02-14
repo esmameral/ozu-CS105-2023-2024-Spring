d9616662deaa33e1f31b046e8292c4ed345d7874
esmameral <esma.meral@gmail.com>
2024-02-14 13:43:16 +0300

    Operator examples

--- FirstJavaProject/src/week1/examples/HelloJavaExample.java
+++ FirstJavaProject/src/week1/examples/HelloJavaExample.java
@@ -1,17 +1,20 @@
 package week1.examples;
 
-public class HelloJava {
+import java.text.SimpleDateFormat;
+import java.lang.System;
+
+public class HelloJavaExample {
 
 	public static void main(String[] args) {
-		double radius=4.5;
+		double radius=6.8;
 				
+		char letter='A';
 		int height=10;
 		double PI=3.14;
 		
 		double volumeSphere=4*PI*radius*radius*radius/3;
 		double volumeCylinder=PI*radius*radius*height;
-		System.out.println(volumeSphere);
-		System.out.println(volumeCylinder);
+		System.out.println(volumeSphere+"\n"+volumeCylinder);
 
 	}
 
---
+++ FirstJavaProject/src/week1/examples/PrimiviteOperatorExamples.java
@@ -0,0 +1,49 @@
+package week1.examples;
+
+public class PrimiviteOperatorExamples {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=7;
+		int c=9;
+		int x=10;
+		int y=1;
+		
+		System.out.println(a==b);
+		System.out.println(a==c);
+		
+		System.out.println(10%3);
+		System.out.println(10/3.0);
+		
+		a=a+1;//a=11
+		a++;//a=12
+		System.out.println(a);
+		
+		b--;//b=b-1
+		System.out.println(b);
+		
+		//c=c+3;
+		c+=3;
+		System.out.println(c);
+		c*=10;//c=c*10
+		System.out.println(c);
+		
+		y=x++;//do assigmnet first then increase value by 1
+		System.out.println(x);
+		System.out.println(y);
+		
+		x=10;
+		y=1;
+		y=++x;//do increament first and then do assigment
+		System.out.println(x);
+		System.out.println(y);
+		
+		int number=123; //  % /
+		
+		System.out.println(number%10);//3
+		System.out.println(number/10);//12
+		
+
+	}
+
+}
