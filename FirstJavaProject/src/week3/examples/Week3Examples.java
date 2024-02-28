01097d27ddeb13e780a289a30d83511c3d69d013
esmameral <esma.meral@gmail.com>
2024-02-28 13:26:44 +0300

    Week3-Exercise-Question2 added

---
+++ FirstJavaProject/src/week3/examples/Week3Examples.java
@@ -0,0 +1,53 @@
+package week3.examples;
+
+public class Week3Examples {
+
+	public static void main(String[] args) {
+		question2();
+
+	}
+
+	//Solution from: Emir Gencay
+	public static void question2() {
+		int[] numbers = {4, 6, 1, 8, 9, 7};
+		System.out.println("3. Index "+numbers[3]);
+		System.out.println("5. Index "+numbers[5]);
+		System.out.println("The size of the array "+numbers.length);
+		for (int i=0;i<numbers.length;i++){
+		    System.out.print(numbers[i]+" ");
+		}
+		System.out.println();
+		for (int number:numbers){
+		    System.out.print(number+" ");
+		}
+	}
+
+	public static void question1() {
+
+	}
+
+	public static void question3() {
+
+	}
+
+	public static void question4() {
+
+	}
+
+	public static void question5() {
+
+	}
+
+	public static void question6() {
+
+	}
+
+	public static void question7() {
+
+	}
+
+	public static void question8() {
+
+	}
+
+}
