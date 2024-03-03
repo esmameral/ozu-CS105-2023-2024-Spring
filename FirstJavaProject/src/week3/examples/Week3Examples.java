732c09bfaf38cf3438e022cd1d533d8168c6dfc7
esmameral <esma.meral@gmail.com>
2024-03-03 23:16:55 +0300

    Week3-Solution of Question 5 and Question 6 added

--- FirstJavaProject/src/week3/examples/Week3Examples.java
+++ FirstJavaProject/src/week3/examples/Week3Examples.java
@@ -7,7 +7,10 @@ public class Week3Examples {
 	public static void main(String[] args) {
 		// question2();
 		// question3();
-		question4();
+		// question4();
+		// question1();
+		question5();
+		//question6();
 
 	}
 
@@ -58,9 +61,9 @@ public class Week3Examples {
 		}
 	}
 
-	//Solution from: Elena Kudret
+	// Solution from: Elena Kudret
 	public static void question4() {
-		int[] numbers = { 1, 5,72 };
+		int[] numbers = { 1, 5, 72 };
 		boolean even = false;
 		for (int i = 0; i < numbers.length; i++) {
 			if (numbers[i] % 2 == 0) {
@@ -73,13 +76,59 @@ public class Week3Examples {
 			System.out.println("No even integers in the array");
 		}
 	}
-	
-	public static void question5() {
 
+	public static void question5() {
+		double[] values=new double[20];
+		values[0]=500;
+		int intCount=1;//because first element is an integer number(500)
+		int lessThan1=0;
+		for (int i = 1; i < values.length; i++) {
+			values[i]=values[i-1]/2;
+			if(values[i]<1)
+				lessThan1++;
+			if(values[i]==(int)values[i])
+				intCount++;
+		}
+		System.out.println(intCount+ " elements are integer");
+		System.out.println(lessThan1+" numbers are less than 1");
 	}
 
+	// Solution from: Elena Kudret
 	public static void question6() {
-
+		Scanner input = new Scanner(System.in);
+		String[] summary=new String[5];
+		int correctCount = 0;
+		int correctAnswer=0;
+		String question;
+		for (int i = 0; i < 5; i++) {
+			int firstNumber = (int) (Math.random() * 10);
+			int secondNumber = (int) (Math.random() * 10);
+			
+			if (firstNumber > secondNumber) {
+				question="What is " + firstNumber + "-" + secondNumber ;
+				correctAnswer=firstNumber-secondNumber;
+			} else {
+				question="What is " + secondNumber + "-" + firstNumber ;
+				correctAnswer=secondNumber-firstNumber;
+			}
+			System.out.println(question+"=?");
+			
+			int userAnswer = input.nextInt();
+			question+="="+userAnswer;
+			if (correctAnswer == userAnswer) {
+				System.out.println("You are correct!");
+				question+=" correct";
+				correctCount++;
+			} else {
+				System.out.println("Your answer is wrong.");
+				question+=" wrong";
+			}
+			summary[i]=question;
+		}
+		System.out.println("Correct	count is " + correctCount);
+		for (int i = 0; i < 5; i++) {
+			System.out.println(summary[i]);
+		}
 	}
 
 	public static void question7() {
