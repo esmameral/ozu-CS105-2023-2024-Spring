f1e4e019d429835d321c7d49e79cc7dbdc678a3d
esmameral <esma.meral@gmail.com>
2024-03-04 11:44:19 +0300

    Week3: solution of question 1 and 8 added

--- FirstJavaProject/src/week3/examples/Week3Examples.java
+++ FirstJavaProject/src/week3/examples/Week3Examples.java
@@ -1,16 +1,19 @@
 package week3.examples;
 
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Week3Examples {
 
 	public static void main(String[] args) {
+		//question1();
 		// question2();
 		// question3();
 		// question4();
-		// question1();
-		question5();
-		//question6();
+		 
+		// question5();
+		// question6();
+		question8();
 
 	}
 
@@ -30,7 +33,17 @@ public class Week3Examples {
 	}
 
 	public static void question1() {
+		for (int i = 5; i >=1; i--) {
+			for(int j=1;j<=5-i;j++) {
+				System.out.print(" ");
+			}
+			
+			for(int j=1;j<=i;j++) {
+				System.out.print("X");
+			}
+			System.out.println();
 
+		}
 	}
 
 	// Solution By: Ata Tan Dagidir
@@ -78,52 +91,52 @@ public class Week3Examples {
 	}
 
 	public static void question5() {
-		double[] values=new double[20];
-		values[0]=500;
-		int intCount=1;//because first element is an integer number(500)
-		int lessThan1=0;
+		double[] values = new double[20];
+		values[0] = 500;
+		int intCount = 1;// because first element is an integer number(500)
+		int lessThan1 = 0;
 		for (int i = 1; i < values.length; i++) {
-			values[i]=values[i-1]/2;
-			if(values[i]<1)
+			values[i] = values[i - 1] / 2;
+			if (values[i] < 1)
 				lessThan1++;
-			if(values[i]==(int)values[i])
+			if (values[i] == (int) values[i])
 				intCount++;
 		}
-		System.out.println(intCount+ " elements are integer");
-		System.out.println(lessThan1+" numbers are less than 1");
+		System.out.println(intCount + " elements are integer");
+		System.out.println(lessThan1 + " numbers are less than 1");
 	}
 
 	// Solution from: Elena Kudret
 	public static void question6() {
 		Scanner input = new Scanner(System.in);
-		String[] summary=new String[5];
+		String[] summary = new String[5];
 		int correctCount = 0;
-		int correctAnswer=0;
+		int correctAnswer = 0;
 		String question;
 		for (int i = 0; i < 5; i++) {
 			int firstNumber = (int) (Math.random() * 10);
 			int secondNumber = (int) (Math.random() * 10);
-			
+
 			if (firstNumber > secondNumber) {
-				question="What is " + firstNumber + "-" + secondNumber ;
-				correctAnswer=firstNumber-secondNumber;
+				question = "What is " + firstNumber + "-" + secondNumber;
+				correctAnswer = firstNumber - secondNumber;
 			} else {
-				question="What is " + secondNumber + "-" + firstNumber ;
-				correctAnswer=secondNumber-firstNumber;
+				question = "What is " + secondNumber + "-" + firstNumber;
+				correctAnswer = secondNumber - firstNumber;
 			}
-			System.out.println(question+"=?");
-			
+			System.out.println(question + "=?");
+
 			int userAnswer = input.nextInt();
-			question+="="+userAnswer;
+			question += "=" + userAnswer;
 			if (correctAnswer == userAnswer) {
 				System.out.println("You are correct!");
-				question+=" correct";
+				question += " correct";
 				correctCount++;
 			} else {
 				System.out.println("Your answer is wrong.");
-				question+=" wrong";
+				question += " wrong";
 			}
-			summary[i]=question;
+			summary[i] = question;
 		}
 		System.out.println("Correct	count is " + correctCount);
 		for (int i = 0; i < 5; i++) {
@@ -136,7 +149,22 @@ public class Week3Examples {
 	}
 
 	public static void question8() {
-
+		int[] arr={2,3,5,12,22,31,0} ;
+		boolean dublicateExists=false;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i+1; j < arr.length; j++) {
+				if(arr[i]==arr[j]) {
+					
+					dublicateExists=true;
+					break;
+				}
+			}
+		}
+		if(dublicateExists)
+			System.out.println("There are duplicate elements");
+		else
+			System.out.println("There are no duplicate elements");
+		
 	}
 
 }
