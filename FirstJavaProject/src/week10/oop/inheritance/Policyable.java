c0b2d9fe457575c90f3db19cae5a7d3fd9188a84
esmameral <esma.meral@gmail.com>
2024-04-17 14:57:10 +0300

    Week10-interface example

--- FirstJavaProject/src/week10/oop/inheritance/Policyable.java
+++ FirstJavaProject/src/week10/oop/inheritance/Policyable.java
@@ -1,5 +1,10 @@
 package week10.oop.inheritance;
 
+import java.util.Date;
+
 public interface Policyable {
 	public void createPolicy();
+	default public Date getEndDate() {
+		return new Date();
+	}
 }
---
+++ FirstJavaProject/src/week10/oop/inheritance/exercise3/MenuItem.java
@@ -0,0 +1,8 @@
+package week10.oop.inheritance.exercise3;
+
+public interface MenuItem {
+	public String getName();
+	/**returns the price of the menu item */
+	public double getPrice();
+
+}
