c3a9891205fe50cb52f7b158a4ee02135c4e7548
esmameral <esma.meral@gmail.com>
2024-05-08 13:52:38 +0300

    Week13-Collection framework, sorting, Collections, Comparator and Comparable examples

--- FirstJavaProject/src/week12/collection/CollectionTest.java
+++ FirstJavaProject/src/week12/collection/CollectionTest.java
@@ -1,39 +1,115 @@
 package week12.collection;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class CollectionTest {
 
 	public static void main(String[] args) {
+
+		// arrayListExample();
+		// treeSetExample();
+		// setExample();
+
+		sortExamples();
 		
-		//arrayListExample();
-		
-		setExample();
-		
+		//comparatorComparableExamples();
 
 	}
+
+	public static void comparatorComparableExamples() {
+		List<Product> productList = new ArrayList<Product>();
+		productList.add(new Product(123, "pencil"));
+		productList.add(new Product(27, "pen"));
+		productList.add(new Product(500, "notebook"));
+		productList.add(new Product(456, "book"));
+		
+		System.out.println("-------ArrayList&before sort---example----");
+		for (int i = 0; i < productList.size(); i++) {
+			System.out.println(productList.get(i));
+		}
+		
+		Collections.sort(productList,new ProductNameComparator());
+		System.out.println("-------ArrayList&after sort by name---example----");
+
+		for (int i = 0; i < productList.size(); i++) {
+			System.out.println(productList.get(i));
+		}
+	}
+
+	public static void sortExamples() {
+		SortedSet<Product> products = new TreeSet<Product>(new ProductNameComparator());
+		products.add(new Product(123, "pencil"));
+		products.add(new Product(27, "pen"));
+		products.add(new Product(27, "pen"));
+		products.add(new Product(500, "notebook"));
+		products.add(new Product(456, "book"));
+		
+
+		
+		System.out.println("-------SortedSet&Comparable----example----");
+		for (Product product : products) {
+			System.out.println(product);
+		}
+
+		List<Product> productList = new ArrayList<Product>();
+		productList.add(new Product(123, "pencil"));
+		productList.add(new Product(27, "pen"));
+		productList.add(new Product(500, "notebook"));
+		productList.add(new Product(456, "book"));
+		System.out.println("-------ArrayList%----example----");
+		for (int i = 0; i < productList.size(); i++) {
+			System.out.println(productList.get(i));
+		}
+		
+		Collections.sort(productList);
+		System.out.println("-------ArrayList-after calling Collections.sort---");
+		for (int i = 0; i < productList.size(); i++) {
+			System.out.println(productList.get(i));
+		}
+	}
+
+	public static void treeSetExample() {
+		SortedSet<String> names = new TreeSet<String>();
+		names.add("zehra");
+		names.add("ayse");
+		names.add("hasan");
+		names.add("kerem");
+		for (String aName : names) {
+			System.out.println(aName);
+
+		}
+	}
+
 	public static void setExample() {
-		Set<String> names =new HashSet<String>();
+		Set<String> names = new HashSet<String>();
 		names.add("Ali");
 		names.add("ayse");
 		names.add("zeki");
+		names.add("zeki");
 		for (String aName : names) {
 			System.out.println(aName);
-			
+
 		}
 	}
-	
+
 	public static void arrayListExample() {
-		ArrayList<String> names =new ArrayList<String>();
+		List<String> names = new ArrayList<String>();
 		names.add("Ali");
 		names.add("ayse");
-		names.add(0,"zeki");
+		names.add("ayse");
+		names.add("Ali");
+		System.out.println(names.size());
+		names.set(1, "hasan");
+		names.add(0, "zeki");
 		for (int i = 0; i < names.size(); i++) {
 			System.out.println(names.get(i));
-			
+
 		}
 	}
 
--- FirstJavaProject/src/week12/collection/Product.java
+++ FirstJavaProject/src/week12/collection/Product.java
@@ -1,16 +1,16 @@
 package week12.collection;
 
-public class Product {
+import java.util.Objects;
+
+public class Product implements Comparable<Product> {
 	private int code;
 	private String name;
-	
+
 	public Product(int code, String name) {
 		super();
 		this.code = code;
 		this.name = name;
 	}
-
-	
 
 	public int getCode() {
 		return code;
@@ -28,23 +28,44 @@ public class Product {
 		this.name = name;
 	}
 
-
-
 	@Override
 	public String toString() {
 		return "Product [code=" + code + ", name=" + name + "]";
 	}
 
+	/**
+	 * Compares this object with the specified object for order. Returns a negative
+	 * integer, zero, or a positive integer as this object is less than, equal to,
+	 * or greater than the specified object.
+	 */
+
+	@Override
+	public int compareTo(Product other) {
+		if (this.getCode() < other.getCode())
+			return -1;
+		else if (this.getCode() > other.getCode())
+			return 1;
+		else
+			return 0;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(code);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return code == other.code;
+	}
 	
 	
-	
-	
+
 }
-
-
-
-
-
-
-
-
